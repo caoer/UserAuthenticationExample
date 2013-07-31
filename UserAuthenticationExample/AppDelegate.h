c8520a80beb7dcf631274ae9237443eab0759c48
Zitao Xiong <caoer115@gmail.com>
2013-07-31 00:01:51 -0400

    Initial Commit

---
+++ UserAuthenticationExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UserAuthenticationExample
+//
+//  Created by Zitao Xiong on 7/31/13.
+//  Copyright (c) 2013 nanaimostudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
